ba86604c55d0757f74f3db01cebe2006c07ea09f
Harish2-9 <144813233+Harish2-9@users.noreply.github.com>
2023-10-02 02:28:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -14,25 +14,25 @@ document.addEventListener("keypress",function(event){makeSound(event.key)})
 
 function makeSound(key){
  switch (key) {
-        case "w":  var au1=new Audio('sounds/tom-1.mp3');
+        case "w":  var au1=new Audio('tom-1.mp3');
                      au1.play();
                      break;
-        case "a":   var au2=new Audio('sounds/tom-2.mp3');
+        case "a":   var au2=new Audio('tom-2.mp3');
                      au2.play();    
                      break;
-        case "s":   var au3=new Audio('sounds/tom-3.mp3');
+        case "s":   var au3=new Audio('tom-3.mp3');
                      au3.play();    
                      break;
-        case "d":   var au4=new Audio('sounds/tom-4.mp3');
+        case "d":   var au4=new Audio('tom-4.mp3');
                      au4.play();    
                      break;
-        case "j":   var au5=new Audio('sounds/crash.mp3');
+        case "j":   var au5=new Audio('crash.mp3');
                      au5.play();    
                      break;
-        case "k":   var au6=new Audio('sounds/kick-bass.mp3');
+        case "k":   var au6=new Audio('kick-bass.mp3');
                      au6.play();    
                      break;
-        case "l":   var au7=new Audio('sounds/snare.mp3');
+        case "l":   var au7=new Audio('snare.mp3');
                      au7.play();    
                      break;                   
         default:
